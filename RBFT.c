c5425c3c9735eb2726338e84c560f216b1930e4b
CarlosChew <33320933+CarlosChew@users.noreply.github.com>
2018-03-15 21:39:11 -0600

    Add files via upload

---
+++ RBFT.c
@@ -0,0 +1,59 @@
+#include "abdrive.h"
+#include "simpletools.h"
+#include "ping.h"
+
+int lap= 0;
+int senFrent = 8;
+
+int main()
+{
+  while(1)
+  {
+    drive_speed(60,60);
+    while(ping_cm(senFrent) > 21) { pause(5); }
+    drive_speed(0,0);
+    
+    go_left();
+    
+    while(lap != 0)
+    {
+      go_right();
+     
+      if(ping_cm(senFrent) < 15)
+      {
+        go_left();
+      }
+      // derecha libre, ir a la derecha
+      else
+      {
+        drive_goto(60,60);
+        continue;
+      }       
+      
+      // revisar frente
+      // si hay algo, la unica opcion es cruzar a la izquierda
+      if(ping_cm(senFrent) < 15)
+      {
+        go_left();
+      }
+      
+      // si el frente esta libre avanzar una revolucion
+      if(ping_cm(senFrent) > 26)
+      { 
+        drive_goto(64,64);
+      }             
+    }    
+  }    
+}
+
+void go_right()
+{
+  drive_goto(26,-25);
+  lap++;
+} 
+
+void go_left()
+{
+  drive_goto(-25,26);  
+  lap--;
+}
